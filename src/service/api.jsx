d92c1185b08b6b1e1204a39fcc32fa9d58bb1079
Ande08 <icotiago258@gmail.com>
2025-01-28 13:28:59 +0200

    atulicao de api a url

--- src/service/api.jsx
+++ src/service/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:'https://118f8a4e-15ea-4977-ba50-d8a5421d500a-00-3tein7el6z18.riker.replit.dev:3000/' 
+    baseURL:'https://118f8a4e-15ea-4977-ba50-d8a5421d500a-00-3tein7el6z18.riker.replit.dev:3000' 
 })
 
 export default api;
